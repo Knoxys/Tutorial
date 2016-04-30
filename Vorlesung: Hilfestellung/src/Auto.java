771ecb0e788f6cfb19575cd4c37ba5dca98d9d6c
Knoxys <Knoxys@25.64.152.187>
2016-04-30 07:47:39 +0200

    Für das Projekt

---
+++ Vorlesung: Hilfestellung/src/Auto.java
@@ -0,0 +1,12 @@
+
+public class Auto {
+
+	int ps;
+	String bezeichnung;
+	
+	public Auto (int ps, String name){
+		this.ps=ps;
+		this.bezeichnung=name;
+				
+	}
+}
---
+++ Vorlesung: Hilfestellung/src/Konto.java
@@ -0,0 +1,14 @@
+
+public class Konto {
+
+	String name;
+	int knr;
+	int guthaben;
+	
+	public Konto (String name, int knr, int guthaben) {
+		this.name=name;
+		this.knr=knr;
+		this.guthaben=guthaben;
+	}
+	
+}
